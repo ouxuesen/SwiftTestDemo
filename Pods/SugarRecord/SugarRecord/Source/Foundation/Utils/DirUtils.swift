a7374327aad7a56ed017ec01e919c11528d79977
ouxuesen <xuesen@lexislive.com.cn>
2018-07-18 11:40:13 +0800

    sync

--- Pods/SugarRecord/SugarRecord/Source/Foundation/Utils/DirUtils.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-func documentsDirectory() -> String {
-    let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
-    return paths[0]
-}
--- Pods/Target Support Files/Pods-SwiftTestTests/Pods-SwiftTestTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftTestTests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftTestTests
-@end
